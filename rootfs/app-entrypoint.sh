9dccc998d4f9d28e8fc297ce9a022202c7ccc5bb
Ivan Ramirez <ivan.ramirez@gmail.com>
2016-09-07 21:34:54 -0400

    Create default gulpfile if missing.
    Add placeholder gulpfile to work as the default one if missing.
    Update entrypoint to create file.

--- rootfs/app-entrypoint.sh
+++ rootfs/app-entrypoint.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+function init_gulp() {
+       cp /conf/default-gulpfile.js gulpfile.js
+}
+
+npm install --save-dev gulp
+npm install
+
+if [ ! -e gulpfile.js ]; then
+	echo "Creating default gulpfile.js"
+	init_gulp
+fi
+
 echo "#########################################################################"
 echo " Running GULP "
 echo "#########################################################################"
 
-npm install --save-dev gulp
-npm install
 gulp
---
+++ rootfs/conf/default-gulpfile.js
@@ -0,0 +1,14 @@
+/**
+ * Default gulpfile.js generated as a placeholder for your project.
+ */
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+
+// define tasks here
+
+gulp.task('default', function() {
+   	// Add tasks here
+   	gutil.log('This is just an empty placeholder gulpfile. Make sure you add your tasks here and restart your container.');
+	gutil.log('For more information visit: https://github.com/gulpjs/gulp');
+});
+
